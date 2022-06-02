26ba61af7da1a082264b3fb56876ffe247eb682b
samned <samuelabebayehu@gmail.com>
2022-06-01 22:11:36 -0700

    description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{   
-    char str1[52];
-    strcpy(str1, "\"Programming is like building a multilingual puzzle");
-    puts(str1);
-    return 0;
+
+/* print using puts */
+
+int main(void)
+{
+	char str1[52];
+
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	puts(str1);
+	return (0);
 }
 
