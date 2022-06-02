9f2ec842832bc871d9caa0cca248939f7688baa1
samned <samuelabebayehu@gmail.com>
2022-06-01 23:05:46 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
