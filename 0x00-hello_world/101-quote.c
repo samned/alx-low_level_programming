876f8b5e85c8c133bf1f9225fd6ab7ee950e10c8
samned <samuelabebayehu@gmail.com>
2022-06-02 00:31:12 -0700

    printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	fprintf(stderr,"%s","and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
+
