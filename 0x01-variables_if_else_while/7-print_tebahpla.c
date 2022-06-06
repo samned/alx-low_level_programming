a18d854214203bec55b27022d0d9682f9db7288c
samned <samuelabebayehu@gmail.com>
2022-06-06 08:04:31 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Check random number's last digit
+ *
+ * Description: Last digit checker
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+	printf('\n');
+	return (0);
+}
