8a89a6848773c25730cf2ad8a1fb20f982b8ce6f
samned <samuelabebayehu@gmail.com>
2022-06-06 07:50:43 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Check random number's last digit
+ *
+ * Description: Last digit checker
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
