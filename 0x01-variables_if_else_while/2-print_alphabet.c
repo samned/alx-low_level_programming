830796f2cf5d0427eda26fc097bec9319b9a30fe
samned <samuelabebayehu@gmail.com>
2022-06-06 07:00:56 -0700

    print lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Check random number's last digit
+ *
+ * Description: Last digit checker
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	return (0);
+}
