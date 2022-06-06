899cb9c572c7e743a7e4e705acf84a1da02f3eec
samned <samuelabebayehu@gmail.com>
2022-06-06 08:17:45 -0700

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Check random number's last digit
+ *
+ * Description: Last digit checker
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i % 10 + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
