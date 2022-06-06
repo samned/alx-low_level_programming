f3bcd5d7de9e98a4f5e7c907e7ef1eba013793da
samned <samuelabebayehu@gmail.com>
2022-06-06 06:20:06 -0700

    betty fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/*
+ * main - Check random number is positive,negative or zero
+ *
+ * Return: 0
+*/
 int main(void)
 {
 	int n;
