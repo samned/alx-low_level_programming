fc002ed83b43784d5f7220177ee5301b56360c60
samned <samuelabebayehu@gmail.com>
2022-06-08 05:23:24 -0700

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - Return last digit
+ *
+ * Description: This program returns last digit
+ *
+ * Return: last digit
+ *
+*/
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar((i * j) / 10 + '0');
+			_putchar((i * j) % 10 + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	return;
+
+}
