de69490b11e1dd3c4cf0e2ed9d1b227169ab5afc
samned <samuelabebayehu@gmail.com>
2022-06-08 04:54:02 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,20 +9,20 @@
 */
 void jack_bauer(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i < 12; i++)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			putchar((i/10)+'0');
-			putchar((i%10)+'0');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(':');
-			putchar((j/10)+'0');
-			putchar((j%10)+'0');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 			putchar('\n');
 		}
 	}
-	return (void);
+	return (0);
 
 }
