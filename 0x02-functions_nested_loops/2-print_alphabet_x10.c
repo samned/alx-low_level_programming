39eba3a75f42766d9bdf89a6e34449a80bd74e83
samned <samuelabebayehu@gmail.com>
2022-06-08 00:47:09 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet - print lower case letters
+ *
+ * Description: This program prints lower case letters
+ *
+ * Return: 0
+ *
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
