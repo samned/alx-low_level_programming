73cb85dd9abf3b9c87a007fa857e83e79645505b
samned <samuelabebayehu@gmail.com>
2022-06-08 02:11:54 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - Return last digit
+ * @n: Int parameter
+ *
+ * Description: This program returns last digit
+ *
+ * Return: last digit
+ *
+*/
+int _abs(int n)
+{
+	return (n % 10);
+}
