fe068f78198ea58d12aea2405bdfb2b4e689aed5
samned <samuelabebayehu@gmail.com>
2022-06-08 00:06:26 -0700

    nested putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print putchar
+ *
+ * Description: This program prints description
+ *
+ * Return: 0
+ *
+*/
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
