c2100fac5b5d887e456926df1d7baaac015cdbe3
samned <samuelabebayehu@gmail.com>
2022-06-08 02:58:16 -0700

    update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * jack_bauer - Return last digit
+ *
+ * Description: This program returns last digit
+ *
+ * Return: last digit
+ *
+*/
+void jack_bauer(void)
+{
+	int i,j;
+
+	for (i = 0; i < 12; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			putchar((i/10)+'0');
+			putchar((i%10)+'0');
+			putchar(':');
+			putchar((j/10)+'0');
+			putchar((j%10)+'0');
+			putchar('\n');
+		}
+	}
+	return (0);
+
+}
