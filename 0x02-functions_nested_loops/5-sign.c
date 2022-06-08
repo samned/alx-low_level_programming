2401654c93753168c17a376cf42e217a32b87ed5
samned <samuelabebayehu@gmail.com>
2022-06-08 02:00:55 -0700

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - print lower case letters
+ * @n: Int parameter
+ *
+ * Description: This program prints lower case letters
+ *
+ * Return: 1 if n greater than 0, 0 if it is 0 and -1 if it is less than 0
+ *
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
