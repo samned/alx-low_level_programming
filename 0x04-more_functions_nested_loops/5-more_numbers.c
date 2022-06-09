539993b004197ac6a57767bc062b807e0614152a
samned <samuelabebayehu@gmail.com>
2022-06-09 07:27:11 -0700

    update

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - 10x numbers
+ *
+ * Description: This is a description
+ *
+ * Return: void
+*/
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j < 9)
+			{
+				_putchar((j % 10) + '0');
+			}
+			else
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
