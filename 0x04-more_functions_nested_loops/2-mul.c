8941fa6b62ba56d2d83ef522b3243849554a20ff
samned <samuelabebayehu@gmail.com>
2022-06-09 05:41:12 -0700

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - Multiply numbers
+ * @a: Int parameter
+ * @b: Int parameter
+ *
+ * Description: This is a description
+ *
+ * Return: 0 or 1
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
