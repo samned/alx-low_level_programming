0a39fd999a7aa259f8ba03401a405229bc524adf
samned <samuelabebayehu@gmail.com>
2022-06-09 05:22:32 -0700

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h";
 /**
  * _isupper - is upper case function
+ * @c: Int parameter
  *
- * Description : This is a description
+ * Description: This is a description
  *
- * Return : 0 or 1
+ * Return: 0 or 1
 */
-int _isupper( int c)
+int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
