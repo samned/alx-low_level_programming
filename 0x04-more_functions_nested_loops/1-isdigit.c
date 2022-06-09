3107c5fc49db73106862b2bff45e2b799bf0c3f5
samned <samuelabebayehu@gmail.com>
2022-06-09 05:32:06 -0700

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - is upper case function
+ * @c: Int parameter
+ *
+ * Description: This is a description
+ *
+ * Return: 0 or 1
+*/
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
