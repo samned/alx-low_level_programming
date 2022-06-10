cbfeb9922a8e339824562d701ab476827b938bd5
samned <samuelabebayehu@gmail.com>
2022-06-10 15:03:55 +0300

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - print_triangle
+ * @size: size
+ * Description: print_triangle
+ *
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j > 0; j--)
+			{
+				if (j <= i)
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
