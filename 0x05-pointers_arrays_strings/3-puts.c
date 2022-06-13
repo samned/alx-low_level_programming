eced1ef9e4f8317eb5cfe7a1b62ed4b4b62ab014
samned <samuelabebayehu@gmail.com>
2022-06-13 17:26:56 +0300

    putchar

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - string length
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_rev - string length
+ * @str: pointer to string
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+}
