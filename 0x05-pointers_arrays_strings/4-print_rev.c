682d55b0aa2a27f57a8e4df981cfc21deb5b86a2
samned <samuelabebayehu@gmail.com>
2022-06-13 18:46:38 +0300

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,22 @@
 #include "main.h"
 /**
  * print_rev - string length
- * @str: pointer to string
+ * @s: pointer to string
  *
  * Return: void
  */
 void print_rev(char *s)
 {
+	int len, i;
+
+	len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	for (i = len; i > 0; i--)
+	{
+		_putchar(s[i - 1]);
+	}
+	_putchar('\n');
 }
