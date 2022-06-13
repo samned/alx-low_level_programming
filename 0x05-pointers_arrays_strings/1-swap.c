afef809b5f32c2aa1d07b74ace799026fb650915
samned <samuelabebayehu@gmail.com>
2022-06-13 12:01:07 +0300

    update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swap two integers
+ * @a: pointer to int
+ * @b: pointer to int
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int *temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
