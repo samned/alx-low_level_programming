77b7b9a4e2d7d3ab06a56a4a0b8497f09c11cf91
samned <samuelabebayehu@gmail.com>
2022-06-16 16:12:10 +0300

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - puts_half
+ *
+ * @str: string parameter
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, i, half;
+
+	len = 0;
+	i = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if ((len / 2) % 2 != 0)
+	{
+		half = len / 2;
+		for (i = half; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,3 +10,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
 void puts2(char *str);
+void puts_half(char *str);
