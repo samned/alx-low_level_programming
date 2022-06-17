2e97f261b541a39d230fdf2aba1947dee562fe3b
samned <samuelabebayehu@gmail.com>
2022-06-17 18:33:29 +0300

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * *_strncpy - *_strncpy
+ * @dest: destination
+ * @src: source
+ * @n: number of chars
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (src[i] != '\0')
+		{
+			dest[i] = src[i];
+		}
+		else
+		{
+			dest[i] = '\0';
+			break;
+		}
+	}
+	return (dest);
+}
