63f833a7e4a3b27eecec77c7c4d3a398d8b81c10
samned <samuelabebayehu@gmail.com>
2022-06-17 20:47:36 +0300

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,18 +9,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int diff, i;
+	int i;
 
-	diff = 0;
 	i = 0;
 	while (s1[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			diff = s1[i] - s2[i];
-			break;
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
-	return (diff);
+	return (0);
 }
