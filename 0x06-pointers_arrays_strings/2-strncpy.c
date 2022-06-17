034684035f84152d55412049e3acd381e8f1daec
samned <samuelabebayehu@gmail.com>
2022-06-17 18:35:28 +0300

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,9 +17,13 @@ char *_strncpy(char *dest, char *src, int n)
 		{
 			dest[i] = src[i];
 		}
-		else
+		else if (src[i] == '\0' && dest[i] != '\0')
 		{
 			dest[i] = '\0';
+			break;
+		}
+		else
+		{
 			break;
 		}
 	}
