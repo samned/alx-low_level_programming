6eeab4f3096f0672a5034e28e56ceafbb76a47e2
samned <samuelabebayehu@gmail.com>
2022-06-24 12:12:36 +0300

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: arguments count
+ * @argv: arguments array
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
