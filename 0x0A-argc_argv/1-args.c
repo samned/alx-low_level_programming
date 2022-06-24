9686bf13b20b4906060166ba87269140dcb6c6db
samned <samuelabebayehu@gmail.com>
2022-06-24 12:21:47 +0300

    args 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: the number of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
