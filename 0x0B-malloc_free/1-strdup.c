3aa9650ff2805f37f9267937aba90f4288173f34
samned <samuelabebayehu@gmail.com>
2022-06-27 13:49:13 +0300

    update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup - strdup
+ * @str: string
+ *
+ * Return: new string
+ */
+char *_strdup(char *str)
+{
+	char *m = malloc(sizeof(str));
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		m[i] = str[i];
+		i++;
+	}
+	return (m);
+}
