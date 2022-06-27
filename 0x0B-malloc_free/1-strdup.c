88df49ef010add210448e9763a7e90cab13e3ecc
samned <samuelabebayehu@gmail.com>
2022-06-27 17:31:19 +0300

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,18 +8,24 @@
  */
 char *_strdup(char *str)
 {
-	char *m = malloc(sizeof(str) + 1);
-	int i;
+	char *m;
+	int i, j;
 
-	if (str == NULL || m == 0)
+	if (str == NULL)
 	{
 		return ('\0');
 	}
-	i = 0;
-	while (str[i] != '\0')
+	for (i = 0; i != '\0'; i++)
+		;
+	m = malloc(i * sizeof(*str) + 1);
+	if (m == 0)
 	{
-		m[i] = str[i];
-		i++;
+		return (NULL);
+	}
+	else
+	{
+		for (j = 0; j < i; j++)
+			m[j] = str[j];
 	}
 	return (m);
 }
