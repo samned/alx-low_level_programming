f46bdfed5142c5cb9a9662e34bd348f9fc4d299e
samned <samuelabebayehu@gmail.com>
2022-06-27 13:09:59 +0300

    malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *create_array - create array
+ * @size: size
+ * @c: charachter
+ *
+ * Return: Char address
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *carr = (char *)malloc(size);
+	int i;
+
+	if (size == 0)
+	{
+		return ('\0');
+	}
+	else
+	{
+		for (i = 0; i < (int)size; i++)
+		{
+			*(carr + i) = c;
+		}
+		return (carr);
+	}
+	return (0);
+}
