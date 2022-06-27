62e311898af1a8ee2cd0f5f9211e32bc3bcdd2ac
samned <samuelabebayehu@gmail.com>
2022-06-27 19:48:10 +0300

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,21 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+/**
+ * _strlen - find length of a string
+ * @s: string
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int size = 0;
+	for (; s[size] != '\0'; size++)
+		;
+	return (size);
+}
+
 /**
  * *str_concat - *str_concat
  * @s1: string 1
@@ -13,18 +28,22 @@ char *str_concat(char *s1, char *s2)
 	char *concat;
 	int size1, size2, i;
 
-	for (size1 = 0; s1[size1] != '\0'; size1++)
-		;
-	for (size2 = 0; s2[size2] != '\0'; size2++)
-		;
-	concat = malloc((size1 * sizeof(*s1)) + (size2 * sizeof(*s2)) + 1);
-	for (i = 0; i < size1; i++)
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	concat = malloc((size1 + size2) * sizeof(char) + 1);
+	if (concat == 0)
+		return (0);
+	for (i = 0; i <= size1 + size2; i++)
 	{
-		concat[i] = s1[i];
+		if (i < size1)
+			concat[i] = s1[i];
+		else
+			concat[i] = s2[i - size1];
 	}
-	for (i = 0; i < size2; i++)
-	{
-		concat[i + size1] = s2[i];
-	}
+	concat[i] = '\0';
 	return (concat);
 }
